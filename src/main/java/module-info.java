38cf404bd23771856c6dd796fc80d9095316974b
EnverJ <Enlem1987@gmail.com>
2024-02-24 23:51:41 -0700

    Big Os

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dsalgoriths.datastructurealgorithms {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens dsalgoriths.datastructurealgorithms to javafx.fxml;
+    exports dsalgoriths.datastructurealgorithms;
+}
